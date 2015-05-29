d778142df09bb4a4cd3909773f054e78c60f3ddf
User0804 <guillaume.dumont@ino.ca>
2015-05-29 17:01:42 -0400

    Fixed build on windows by adding unistd.h and getopt.h/.c

--- libraries/liblmdb/mdb.c
+++ libraries/liblmdb/mdb.c
@@ -96,7 +96,11 @@ extern int cacheflush(char *addr, int nbytes, int cache);
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#ifdef _MSC_VER
+#include "unistd.h"
+#else
 #include <unistd.h>
+#endif
 
 #if defined(__sun) || defined(ANDROID)
 /* Most platforms have posix_memalign, older may only have memalign */
